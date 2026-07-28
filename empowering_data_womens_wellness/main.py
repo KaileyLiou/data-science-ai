4b21bc84f941d3c26a288c88d624fe7307c3c0e9
Kailey Liou <kaileyliou09@gmail.com>
2026-07-28 16:21:14 -0400

    completed part 2

--- empowering_data_womens_wellness/main.py
+++ empowering_data_womens_wellness/main.py
@@ -8,28 +8,10 @@ import GWCutilities as util
 
 # Read a comma separated values (CSV) files into a variable
 # as a pandas DataFrame
-lwd=pd.read_csv("livwell135.csv")
+# Step 1: If you wish chane which data set you are working with, do that here: 
+lwd=pd.read_csv("livwell175.csv")
 
-# Print out the number of rows and columns
-print(lwd.shape)
+# Add Step 2 code here:
+print(util.vformat_list(lwd.columns))
 
-#  basic colors:
-# 'blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'white'
-
-# create a scatter plot
-plt.scatter(lwd["year"],lwd["EI_women_fridge_p"],color="green")
-
-# add a title to the plot
-plt.title("Percent of Women With a Fridge")
-
-#Label the x-axis
-plt.xlabel("Year")
-
-# label the y-axis
-plt.ylabel("Women with a fridge (%)")
-
-# set the range for the y-axis
-plt.ylim(0,14)
-
-# show the plot
-plt.show()
+# 
