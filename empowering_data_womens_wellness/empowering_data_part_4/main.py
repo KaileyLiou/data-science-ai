884685186058c3748c34836156b065fd06161886
Kailey Liou <kaileyliou09@gmail.com>
2026-07-31 00:15:28 -0400

    plot 5 columns

--- empowering_data_womens_wellness/empowering_data_part_4/main.py
+++ empowering_data_womens_wellness/empowering_data_part_4/main.py
@@ -8,13 +8,23 @@ import GWCutilities as util
 
 # Read a comma separated values (CSV) files into a variable
 # as a pandas DataFrame
-lwd=pd.read_csv("livwell135.csv")
+lwd=pd.read_csv("livwell175.csv")
 
 #Change x here:
-plt.hist(x='HD_women_size_mean', data=lwd, edgecolor='white', bins=10)
+# plt.hist(x='HD_women_size_mean', data=lwd, edgecolor='white', bins=10)
+# plt.hist(x='ED_litt_p', data=lwd, edgecolor='white', bins=10)
+# plt.hist(x='WL_wealth_mean', data=lwd, edgecolor='white', bins=10)
+# plt.hist(x='DP_decide_money_p', data=lwd, edgecolor='white', bins=10)
+# plt.hist(x='DV_phys_partner_p', data=lwd, edgecolor='white', bins=10)
+plt.hist(x='RH_children_born_mean', data=lwd, edgecolor='white', bins=10)
 
 #Change x-label here:
-plt.xlabel("Average number of household members")
+# plt.xlabel("Average number of household members")
+# plt.xlabel("Women who are literate (%)")
+# plt.xlabel("Average of the International Wealth Index")
+# plt.xlabel("Women currently married or in union who were paid cash for their work, who decide alone how their money earned is spent (%)")
+# plt.xlabel("Women who ever experienced physical violence by partner (%)")
+plt.xlabel("Average number of children ever born")
 
 plt.ylabel("Number of Data Points")
 plt.show()
