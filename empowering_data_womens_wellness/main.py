45847bf28d536ed39b96c8f5ac72bd4f499be199
Kailey Liou <kaileyliou09@gmail.com>
2026-08-02 12:26:10 -0400

    finished empowering data project

--- empowering_data_womens_wellness/main.py
+++ empowering_data_womens_wellness/main.py
@@ -12,6 +12,16 @@ import GWCutilities as util
 #If you wish change which data set you are working with, do that here: 
 lwd=pd.read_csv("livwell175.csv")
 
+print("Vietnam and Egypt are both lower-middle-income countries, but they have taken different paths in education and economic development.\n")
+print("This data represents women living in these two countries and compares female literacy with average household wealth.\n")
+input("Press return to continue.\n")
+
+print("While exploring the data, I noticed that women in Vietnam consistently have high literacy rates, even though Vietnam has a lower average wealth index than Egypt.\n")
+print("In Egypt, the average wealth index is generally higher, but female literacy varies much more across the data.\n")
+
+print("Based on these findings, my proposed research question is:\n")
+print("Why does Vietnam maintain consistently high female literacy despite having a lower average International Wealth Index than Egypt?\n")
+
 vietnamBooleanList = lwd["country_name"]=="Vietnam"
 vietnamData = lwd.loc[vietnamBooleanList]
 
