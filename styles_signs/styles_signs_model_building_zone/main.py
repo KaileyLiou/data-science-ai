1a5849bd6aed730ed1de61f8c86666a15878883c
Kailey Liou <kaileyliou09@gmail.com>
2026-08-04 20:06:28 -0400

    removed test dataset and fixed output label

--- styles_signs/styles_signs_model_building_zone/main.py
+++ styles_signs/styles_signs_model_building_zone/main.py
@@ -14,16 +14,7 @@ y_train = train_data['class']   # Target (first column)
 
 X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train, random_state=2, test_size=20, stratify=y_train)
 
-# Load the testing dataset
-test_data = pd.read_csv('fashion_mnist_20bal_test.csv')
-
-# Separate the test data (features) and the  classes
-X_test = test_data.drop('class', axis=1)  # Features (all columns except the first one)
-X_test = X_test / 255.0
-y_test = test_data['class']   # Target (first column)
-
-
-neural_net_model = MLPClassifier( hidden_layer_sizes=(8),random_state=42,tol=0.005)
+neural_net_model = MLPClassifier(hidden_layer_sizes=(8),random_state=42,tol=0.005)
 
 neural_net_model.fit(X_train, y_train)
 # Determine model architecture 
@@ -32,10 +23,9 @@ layer_sizes += [coef.shape[1] for coef in neural_net_model.coefs_]  # Add sizes 
 layer_size_str = " x ".join(map(str, layer_sizes))
 print(f"Layer sizes: {layer_size_str}")
 
-
 # predict the classes from the training and test sets
 y_pred_train = neural_net_model.predict(X_train)
-y_pred = neural_net_model.predict(X_test)
+y_pred = neural_net_model.predict(X_validate)
 
 # Create dictionaries to hold total and correct counts for each class
 correct_counts = defaultdict(int)
@@ -43,7 +33,7 @@ total_counts = defaultdict(int)
 overall_correct = 0
 
 # Count correct test predictions for each class
-for true, pred in zip(y_test, y_pred):
+for true, pred in zip(y_validate, y_pred):
     total_counts[true] += 1
     if true == pred:
         correct_counts[true] += 1
@@ -57,13 +47,12 @@ for true, pred in zip(y_train, y_pred_train):
     if true == pred:
         correct_counts_training += 1
 
-
 # Calculate and print accuracy for each class and overall test accuracy
 for class_id in sorted(total_counts.keys()):
     accuracy = correct_counts[class_id] / total_counts[class_id] *100
     print(f"Accuracy for class {class_id}: {accuracy:3.0f}%")
 print(f"----------")
-overall_accuracy = overall_correct / len(y_test)*100
-print(f"Overall Test Accuracy: {overall_accuracy:3.1f}%")
+overall_accuracy = overall_correct / len(y_validate)*100
+print(f"Overall Validation Accuracy: {overall_accuracy:3.1f}%")
 overall_training_accuracy = correct_counts_training / total_counts_training*100
 print(f"Overall Training Accuracy: {overall_training_accuracy:3.1f}%")
